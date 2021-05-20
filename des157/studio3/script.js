d6861d3201ead8977f4dd79fbce26756184b5896
Ama Dadzie <asdadzie@ucdavis.edu>
2021-05-20 15:31:26 -0700

    added stand to indicate player turn

--- des157/studio3/script.js
+++ des157/studio3/script.js
@@ -2,18 +2,20 @@
     "use strict";
 
     const startOverlay = document.getElementById('start-overlay');
-    const startGame = document.getElementById('start');
-    const gameArea = document.getElementById('gamearea');
+    const startBtn = document.getElementById('start-btn');
     const gameRoll = document.getElementById('gameroll');
     const gameText = document.getElementById('gametext');
     const player1 = document.querySelectorAll('#player1 p');
     const player2 = document.querySelectorAll('#player2 p');
     const die = document.querySelectorAll('#gamearea img');
+    const rollBtn = document.getElementById('roll-btn');
+    const passBtn = document.getElementById('pass-btn');
+
    
 
     let gameData = {
         dice: ['1-cube.png', '2-cube.png','3-cube.png','4-cube.png','5-cube.png','6-cube.png' ],
-        players: ['player1', 'player2'],
+        players: ['Player 1', 'Player 2'],
         score: [0,0],
         roll1: 0,
         roll2: 0,
@@ -22,116 +24,123 @@
         gameEnd: 29
     };
 
-    startGame.addEventListener('click', function(){
+    startBtn.addEventListener('click', function(){
         gameData.index = Math.round(Math.random());
-        // gameControl.innerHTML = '<h2>The Game Has Started</h2>';
-        // gameControl.innerHTML += '<button id="quit">Wanna Quit?</button>';
-
-        document.getElementById('quit').addEventListener('click', function(){
+        document.getElementById('quit-btn').addEventListener('click', function(){
             location.reload();
         });
+        
+        const p1 = document.getElementById('p1name').value;
+        const p2 = document.getElementById('p2name').value;
+        
+        if(p1 && p2){
+            gameData.players[0] = p1;
+            gameData.players[1] = p2;
+            player1[1].textContent = gameData.players[0];
+            player2[1].textContent = gameData.players[1];
+        }
 
-        gameData.players[0] = document.getElementById('p1name').value;
-        gameData.players[1] = document.getElementById('p2name').value;
-        player1[1].textContent = gameData.players[0];
-        player2[1].textContent = gameData.players[1];
         startOverlay.style.display = 'none';
         die[0].style.visibility = 'hidden';
         die[1].style.visibility = 'hidden';
         initialRoll()
-        setUpTurn();
+        displayTurn();
     })
 
     /* ADD EVENT LISTENER FOR ROLL */ 
     function initialRoll(){
-        document.getElementById('roll').addEventListener('click', function(){
+        rollBtn.addEventListener('click', function(){
             throwDice();
+        })
+
+        passBtn.addEventListener('click', function(){
+            gameData.index ? (gameData.index = 0) : (gameData.index = 1);
+            displayTurn();
         })
 
     }
 
-    function setUpTurn(){
+    function displayTurn(){
         gameText.textContent = `Roll the dice for the ${gameData.players[gameData.index]}`;
+        let currPlayerImg = document.querySelector(`#player${gameData.index + 1} img`);
+        currPlayerImg.src = 'images/stand.png';
+        currPlayerImg.alt = 'rubiks cube stand';
+
+        let index = gameData.index;
+        index ? (index = 0) : (index = 1);
+        let otherPlayerImg = document.querySelector(`#player${index + 1} img`);
+        otherPlayerImg.src = '';
+        otherPlayerImg.alt = '';
     }
 
     /* ROLL THE DICE */ 
     function throwDice(){
-        console.log('IN THROW DICE')
-        //actionArea.innerHTML = '';
         die[0].style.visibility = 'visible';
         die[1].style.visibility = 'visible';
 
         gameData.roll1 = Math.floor(Math.random() * 6) + 1;
         gameData.roll2 = Math.floor(Math.random() * 6) + 1;
+
         gameRoll.textContent  = ` ${gameData.roll1} - ${gameData.roll2}`;
-        //gameText.textContent  = `Roll the dice for ${gameData.players[gameData.index]}`;
         die[0].src = `images/${gameData.dice[gameData.roll1-1]}`;
         die[1].src = `images/${gameData.dice[gameData.roll2-1]}`;
-
-
         
         gameData.rollSum = gameData.roll1 + gameData.roll2;
 
-        console.log('player', gameData.players[gameData.index]);
-        console.log('roll 1',gameData.roll1 );
-        console.log('roll 2',gameData.roll2 );
-        console.log('sum',gameData.rollSum);
-
         if(gameData.rollSum === 2){
-            console.log('TWO ONES')
-            gameText.textContent = 'You rolled two ones!';
+            gameText.textContent = 'Sorry! You rolled two ones!';
             gameData.score[gameData.index] = 0;
             gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-            setTimeout(setUpTurn, 3000);
+            showCurrentScore();
+            setTimeout(displayTurn, 2000);
             
         } else if (gameData.roll1 ===1 || gameData.roll2 ===1){
-            console.log('ONE')
             gameData.index ? (gameData.index = 0) : (gameData.index = 1);
             gameText.textContent = `Sorry, one of your rolls was a one, switching to ${gameData.players[gameData.index]}`;
-            setTimeout(setUpTurn, 3000);
+            setTimeout(displayTurn, 2000);
             
 
         } else {
             console.log('NO ONES')
             gameData.score[gameData.index] = gameData.score[gameData.index] + gameData.rollSum;
-            document.getElementById('pass').addEventListener('click', function(){
-                gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-                setUpTurn();
-            })
            
         }
         checkWinningCondition();
     }
 
-      /* CHECK IF WINNING CONDITION IS MET */ 
+    /* CHECK IF WINNING CONDITION IS MET */ 
     function checkWinningCondition(){
         if(gameData.score[gameData.index] > gameData.gameEnd){
             showCurrentScore();
-            gameText.textContent  = `${gameData.players[gameData.index]} wins with ${gameData.score[gameData.index]} points!`;
-            gameText.style.fontFamily = "'Fascinate', cursive";
-            const quit = document.getElementById('quit')
-            quit.textContent = 'Start a New Game?';
-            quit.style.width = '300px';
+            displayWinner();
 
-            document.getElementById('pass').style.display = 'none';
-            document.getElementById('roll').style.display = 'none';
-            gameRoll.style.display = 'none';
-
-            die[0].src = 'images/colored-cube.png';
-            die[1].src = 'images/colored-cube.png';
         } else {
             showCurrentScore();
            
         }
     }
 
+    /* SAY WINNER NAME AND REMOVE ROLL & PASS BTNS */ 
+    function displayWinner(){
+        gameText.textContent  = `${gameData.players[gameData.index]} wins with ${gameData.score[gameData.index]} points!`;
+
+        gameText.style.fontFamily = "'Fascinate', cursive";
+        const quit = document.getElementById('quit-btn')
+        quit.textContent = 'Start a New Game?';
+        quit.style.width = '300px';
+
+        passBtn.style.display = 'none';
+        rollBtn.style.display = 'none';
+        gameRoll.style.display = 'none';
+
+        die[0].src = 'images/colored-cube.png';
+        die[1].src = 'images/colored-cube.png';
+    }
+
    /* SHOW PLAYER SCORE */ 
     function showCurrentScore(){
-        if(gameData.index === 0){
             player1[0].textContent = gameData.score[0]
-        } else {
             player2[0].textContent = gameData.score[1]
-        }
         
     }
 
