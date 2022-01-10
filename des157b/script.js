0c17f3009d6d1faa0761b6c2817f4ebdf9bbbc45
Ama Dadzie <asdadzie@ucdavis.edu>
2022-01-09 21:55:13 -0800

    added music not images and added javascript to hide and display them

--- des157b/script.js
+++ des157b/script.js
@@ -6,6 +6,7 @@
     const banner = document.querySelector('#banner'); 
     const sectionHeaders = document.querySelectorAll('section h3')
     const sections = document.querySelectorAll('section');
+    const musicImages = document.querySelectorAll('h3 img');
     let mode = 'light';
     const blueColors = ['#1999D0', '#2EAFE7',' #6BCBF3',' #AEE1F7'];
     const rainbowColors = ['#FA2B2B', '#F0AE2F', '#FDEA43', '#54E231', '#269FF6','#3749EE','#9736E3'];
@@ -24,6 +25,9 @@
             }
             buttonImg.src = 'images/wave.png';
             changeBarColor(rainbowColors);
+            musicImages.forEach(function(eachImg){
+                eachImg.src = 'images/music-note-red.png';
+            });
 
            /*  let headerColor = rainbowColors[randomInt(0,7)];
             sectionHeaders.forEach(function(header){
@@ -39,6 +43,9 @@
             }
             buttonImg.src = 'images/rainbow.png';
             changeBarColor(blueColors);
+            musicImages.forEach(function(eachImg){
+                eachImg.src = 'images/music-note-blue.png';
+             });
 
             /* sectionHeaders.forEach(function(header){
                 header.style.color = '#1999D0';
@@ -48,24 +55,10 @@
     })
 
     window.addEventListener('load', function(){
-        // let group = randomInt (4, 10);
-        // let groupCount = 1;
         for(let i =0; i <75; i++){
             let bar = document.createElement('div');
             let height = randomInt (4, 250);
-
-            // if(groupCount == group){
-            //     group = randomInt (4, 10);
-            //     groupCount = 1;
-            //     colorCount++;
-            // }
-
-            // if(colorCount == 3){
-            //     colorCount = 0;
-            // }
-        
             bar.setAttribute('style', `height: ${height}px`); //background-color: ${color[colorCount]}`
-            // groupCount++;
             banner.appendChild(bar);
         }
         changeBarColor(blueColors);
@@ -100,6 +93,42 @@
     function animationDelay(delay, div ){
         div.style.animationDelay = `${delay}s`;
     }
+
+    const allSectionInfo = [];
+    sections.forEach(function(eachSection){
+       allSectionInfo.push(eachSection.getBoundingClientRect());
+    });
+    console.log(allSectionInfo)
+
+    function musicNoteHover(mouseX, mouseY){
+        if(mouseX >= allSectionInfo[0].x && mouseX <=allSectionInfo[0].right && mouseY >=allSectionInfo[0].y && mouseY <=allSectionInfo[0].bottom){
+            musicImages[0].style.display = 'inline';
+            musicImages[1].style.display = 'inline';
+        } else {
+            musicImages[0].style.display = 'none';
+            musicImages[1].style.display = 'none';
+        }
+        if(mouseX >= allSectionInfo[1].x && mouseX <=allSectionInfo[1].right && mouseY >=allSectionInfo[1].y && mouseY <=allSectionInfo[1].bottom){
+            musicImages[2].style.display = 'inline';
+        } else {
+            musicImages[2].style.display = 'none';
+        }
+        if(mouseX >= allSectionInfo[2].x && mouseX <=allSectionInfo[2].right && mouseY >=allSectionInfo[2].y && mouseY <=allSectionInfo[2].bottom){
+            musicImages[3].style.display = 'inline';
+        } else{
+            musicImages[3].style.display = 'none';
+        }
+        if(mouseX >= allSectionInfo[3].x && mouseX <=allSectionInfo[3].right && mouseY >=allSectionInfo[3].y && mouseY <=allSectionInfo[3].bottom){
+            musicImages[4].style.display = 'inline';
+        } else {
+            musicImages[4].style.display = 'none';
+        }
+    }
+
+    window.addEventListener('mousemove', function(event){
+        setInterval(musicNoteHover(event.clientX, event.clientY), 1000);
+       
+    })
 
     function randomInt(min, max) { // min and max included 
         return Math.floor(Math.random() * (max - min + 1) + min)
