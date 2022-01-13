e5f4ffcf2a8037cfca4e214f2aa830db3e457a23
Ama Dadzie <asdadzie@ucdavis.edu>
2022-01-13 10:50:36 -0800

    added blockbuster video folder and created loading content

---
+++ des157b/blockbuster/script.js
@@ -0,0 +1,10 @@
+(function(){
+    'use strict';
+
+    const loadingSection = document.getElementById('loading');
+    
+    loading.addEventListener('mouseover', function(){
+        loadingSection.style.animation = 'fadeAway 1s linear';
+    })
+
+})();
