7c404bf7cad98756fd9ec17eb94f6685a41fb466
Ama Dadzie <asdadzie@ucdavis.edu>
2022-02-04 20:19:03 -0800

    created folder for library liberty and written html and most of the css, added a library

---
+++ des157b/library-liberty/script.js
@@ -0,0 +1,34 @@
+tippy('#cont2008', {
+    content: 'moved to california',
+    theme: 'light',
+    animation: 'scale-subtle',
+    size: 'large'
+
+
+});
+
+tippy('#cont2015', {
+    content: 'adopted rocket!',
+    theme: 'light',
+    animation: 'scale-subtle',
+    offset: [0, 20]
+
+
+});
+
+tippy('#cont2016', {
+    content: 'portland & voodoo donuts!',
+    theme: 'light',
+    animation: 'scale-subtle',
+    // placement: 'right-end'
+    // offset: [0, 20]
+
+});
+
+tippy('#img2021', {
+    content: 'visited oregon',
+    theme: 'light',
+    animation: 'scale-subtle'
+    // offset: [0, 20]
+
+});
