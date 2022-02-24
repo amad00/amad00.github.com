9efef7e56a56138ce0f387021d305f3fe1022107
Ama Dadzie <asdadzie@ucdavis.edu>
2022-02-24 11:33:58 -0800

    added background interaction

--- des157b/final/version2/script.js
+++ des157b/final/version2/script.js
@@ -60,3 +60,57 @@
     })
     
 })()
+
+
+// BACKGROUND
+
+
+
+// this class describes the properties of a single particle.
+class Particle {
+    // setting the co-ordinates, radius and the
+    // speed of a particle in both the co-ordinates axes.
+      constructor(){
+        this.x = random(0,width);
+        this.y = random(0,height);
+        this.r = random(1,10);
+        this.xSpeed = random(-0.5,0.5);
+        this.ySpeed = random(-0.5,0.5);
+      }
+    
+    // creation of a particle.
+      createParticle() {
+        noStroke();
+        fill('rgba(56,81,103,0.75)');
+        circle(this.x,this.y,this.r);
+      }
+    
+    // setting the particle in motion.
+      moveParticle() {
+        if(this.x < 0 || this.x > width)
+          this.xSpeed*=-1;
+        if(this.y < 0 || this.y > height)
+          this.ySpeed*=-1;
+        this.x+=this.xSpeed;
+        this.y+=this.ySpeed;
+      }
+    }
+    // an array to add multiple particles
+    let particles = [];
+    
+    function setup() {
+      createCanvas(windowWidth, windowHeight);
+      for(let i = 0;i<width/10;i++){
+        particles.push(new Particle());
+      }
+    }
+    
+    function draw() {
+      background('#0a1b2b');
+      for(let i = 0;i<particles.length;i++) {
+        particles[i].createParticle();
+        particles[i].moveParticle();
+        
+      }
+    }
+    
