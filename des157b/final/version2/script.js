8905cec310e828c996d572456eca7a348202f19e
Ama Dadzie <asdadzie@ucdavis.edu>
2022-02-27 16:35:37 -0800

    added database information and more styling

--- des157b/final/version2/script.js
+++ des157b/final/version2/script.js
@@ -1,4 +1,7 @@
-(function(){
+Parse.initialize("H2YRuCmKc8rIRzwcAUHNomzPtjKlwb2FQicjGV9t","xFnT8tgOuksTxhBlLFjTuGlHq11jRAdyeUpdKN3d");
+Parse.serverURL = 'https://parseapi.back4app.com/';
+
+// (function(){
     'use strict';
     const participateBtn = document.querySelector('#welcome-btns button');
     const galleryBtn = document.querySelectorAll('#welcome-btns button')[1];
@@ -14,9 +17,11 @@
     const forms = document.querySelectorAll('form');
     const gallerySection = document.querySelector('#gallery');
 
+
     galleryBtn.addEventListener('click', function() {
         welcomeSection.style.display = 'none';
-        gallerySection.style.display = 'flex';
+        // gallerySection.style.display = 'flex';
+        displayGallery();
     })
 
     participateBtn.addEventListener('click', function() {
@@ -51,20 +56,101 @@
         forms[4].style.display = 'none';
         questions.style.display = 'none';
         quotes[1].style.display = 'flex';
+       
     })
 
     viewGalleryBtn.addEventListener('click', function(event) {
         event.preventDefault();
-        quotes[1].style.display = 'none';
-        gallerySection.style.display = 'flex';
+        // quotes[1].style.display = 'none';
+        // gallerySection.style.display = 'flex';
+        addResponse();
     })
     
-})()
+  
 
+// DATABASE
+async function addResponse(){
 
+  const inputs = document.querySelectorAll('.responses');
+  const newResponse = {};
+  
+  for(let i=0; i<inputs.length; i++){
+    let key = inputs[i].getAttribute('name');
+    let value = inputs[i].value;
+    newResponse[key] = value;
+  }
+
+  const newResponseData = new Parse.Object("Responses");
+  newResponseData.set("q1", newResponse.q1);
+  newResponseData.set("q2", newResponse.q2);
+  newResponseData.set("q3", newResponse.q3);
+  newResponseData.set("q4", newResponse.q4);
+  newResponseData.set("q5", newResponse.q5);
+
+  try{
+    await newResponseData.save();
+    quotes[1].style.display = 'none';
+    displayGallery();
+    // resetFormFields();
+  } catch(error) {
+    console.error('Error while creating response: ', error);
+  }
+
+}
+
+async function displayGallery(){
+  const responses = Parse.Object.extend('Responses');
+  const query = new Parse.Query(responses);
+
+  try {
+    const results = await query.descending('createdAt').find();
+    let count = 0;
+   
+    results.forEach(function(eachResponse) {
+      const id = eachResponse.id;
+      const q1 = eachResponse.get('q1');
+      const q2 = eachResponse.get('q2');
+      const q3 = eachResponse.get('q3');
+      const q4 = eachResponse.get('q4');
+      const q5 = eachResponse.get('q5');
+      
+      const thedDivItem = document.createElement('div');
+      thedDivItem.setAttribute('class', 'gallery-item');
+      const thePItem = document.createElement('p');
+      thePItem.textContent = q5;
+      thedDivItem.append(thePItem);
+      gallerySection.append(thedDivItem);
+
+      
+      count++;
+      if (results.length >6){
+        if(count == results.length - 6){
+        const div = document.createElement('div');
+        div.setAttribute('id', 'gallery-quote');
+        const quote = document.createElement('h3');
+        quote.textContent= "";
+        const author = document.createElement('h3');
+        author.textContent="- ";
+
+        div.append(quote);
+        div.append(author);
+        gallerySection.append(div);
+        }
+      }
+    });
+
+   
+    gallerySection.style.display = 'flex';
+
+    
+  } catch(error){
+    console.log('Error while fetching responses', error);
+
+  }
+}
+
+// })();
 // BACKGROUND
-
-
 
 // this class describes the properties of a single particle.
 class Particle {
