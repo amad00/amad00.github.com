9c034c694eec26df39280e46a43058091763013f
Ama Dadzie <asdadzie@ucdavis.edu>
2022-03-02 21:58:43 -0800

    added styling to button and project images and added aos library

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,14 +1,16 @@
 (function(){
     'use strict';
 
+    AOS.init();
+    
     const body = document.querySelector('body');
     const headerBackground = document.querySelector('#header-background');
     const header =  document.querySelector('header');
     const navIcon = document.querySelector('#nav-icon');
     const nav = document.querySelector('nav');
+    const navLinks = document.querySelectorAll('nav ul li');
 
     let navOpen = false;
-
     let scrollValue = window.scrollY;
 
     navIcon.addEventListener('click', function(){
@@ -30,6 +32,18 @@
             
         }
 
+    });
+
+    navLinks.forEach(function(eachLink){
+        eachLink.addEventListener('click', function(){
+            headerBackground.classList.remove('open');
+            header.classList.remove('open');
+            navIcon.classList.remove('open'); 
+            nav.style.display = 'none';
+            body.style.overflow = 'initial';
+            navOpen = false;
+
+        })
     });
 
     window.addEventListener('scroll', function() {
