402531e89e208cf41ecd3ab692f91708e1050bb3
Ama Dadzie <asdadzie@ucdavis.edu>
2022-03-08 11:22:15 -0800

    changed overlay bg color

--- des157b/final/final-project/script.js
+++ des157b/final/final-project/script.js
@@ -473,6 +473,7 @@ Parse.serverURL = "https://parseapi.back4app.com/";
                 timeTags[1].textContent = `${newDate[1]}`;
 
                 overlayBg.style.height = "100vh";
+                overlayBg.style.backgroundColor = "red";
                 overlayBg.className = "show";
                 overlay.className = "show";
                 document.querySelector("body").style.overflow = "hidden";
