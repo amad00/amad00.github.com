2941bf3178fe4963dfd96c21de8379d4dd47dc8c
Ama Dadzie <asdadzie@ucdavis.edu>
2022-03-08 19:37:19 -0800

    added grid for ipad

--- des157b/final/final-project/script.js
+++ des157b/final/final-project/script.js
@@ -477,7 +477,7 @@ Parse.serverURL = "https://parseapi.back4app.com/";
                 timeTags[1].textContent = `${newDate[1]}`;
 
                 overlayBg.style.height = "100vh";
-                document.querySelector("body").style.backgroundColor = "green";
+                document.querySelector("body").style.backgroundColor = "blue";
                 overlayBg.className = "show";
                 overlay.className = "show";
                 document.querySelector("body").style.overflow = "hidden";
